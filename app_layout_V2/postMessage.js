aa645c61a9ffcd57ea821c1ce50719c8e7297a7f
BKhurshid <josephsun4@gmail.com>
2015-11-16 19:54:26 -0800

    Added the submit function, template from other work.

--- app_layout_V2/postMessage.js
+++ app_layout_V2/postMessage.js
@@ -1,29 +1,28 @@
-ngular.module("froogie")
-    .controller("MessageCrl", ['$rootScope','$scope', '$http', messageGet]);
+angular.module("froogie")
+    .controller('submitCtrl',['$rootScope','$scope', '$http', function($rootScope,$scope,$http) {
+//submit function for new user
+      $scope.submit = function() {
+        //if there is text
+        if ($scope.text) {
+          //increment id
+          num++;
+          //split the name and email
+          var data = $scope.text.split(' ');
 
-    function messageGet($rootScope,$scope, $http) {
-        $scope.getHackReactor = function() {
-      //Make a get request to the server
-        $http.get("http://localhost:3000/index/hackreactor")
-        //if success then
-          .then(function (response) {
-            //array of user information. Unnecessary as we will have a database
-              $scope.display = [];
-              //loop through users information. This assumes the data we want is in response.data
-//f
-              for (var i = 0; i < response.data.length; i++){
-                //format user's information. Assumes the id, name, email are properties. Can modify, will need proprety of message  however that users data is going to be formated. 
-                var wholeUser = "Id:" + response.data[i].id +" Name: " + response.data[i].name + " Email: " + response.data[i].email ;
-                //store user's information. This will be unnecessary as we have a database. 
-                $scope.display.push(wholeUser);
-              }
-              
-      //if error then
-          }, function(error){
-                  console.log("error message:", error);
-        });
-
+          informationToSend = {
+            email: data[1], 
+            id: num,
+            name: data[0]
+          };
+          //send the informatioon to the server
+        $http.post('http://localhost:3000/api/users', JSON.stringify(informationToSend));
+        ///empty the text box
+        $scope.text = '';
         }
+      };
+    }]);
+
+
 
 
 }
