dda6ea7a84c30b2962b75d555cba9a460c4e6682
BKhurshid <josephsun4@gmail.com>
2015-11-16 19:41:24 -0800

    Building http request.

---
+++ app_layout_V2/messageReq.js
@@ -0,0 +1,29 @@
+angular.module("froogie")
+    .controller("MessageCrl", ['$rootScope','$scope', '$http', messageGet]);
+
+    function messageGet($rootScope,$scope, $http) {
+        $scope.getHackReactor = function() {
+      //Make a get request to the server
+        $http.get("http://localhost:3000/index/hackreactor")
+        //if success then
+          .then(function (response) {
+            //array of user information
+              $scope.display = [];
+              //loop through users information. This assumes the data we want is in response.data
+
+              for (var i = 0; i < response.data.length; i++){
+                //format user's information. Assumes the id, name, email are properties. Can modify, will need proprety of message  however that users data is going to be formated. 
+                var wholeUser = "Id:" + response.data[i].id +" Name: " + response.data[i].name + " Email: " + response.data[i].email ;
+                //store user's information
+                $scope.display.push(wholeUser);
+              }
+              
+      //if error then
+          }, function(error){
+                  console.log("error message:", error);
+        });
+
+        }
+
+
+}
