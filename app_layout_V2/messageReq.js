e1ad9c7dc467523e82b17342e408e6fae5bec18a
BKhurshid <josephsun4@gmail.com>
2015-11-16 19:48:35 -0800

    Added psuedo code on what is necessary and what is not

--- app_layout_V2/messageReq.js
+++ app_layout_V2/messageReq.js
@@ -7,14 +7,14 @@ angular.module("froogie")
         $http.get("http://localhost:3000/index/hackreactor")
         //if success then
           .then(function (response) {
-            //array of user information
+            //array of user information. Unnecessary as we will have a database
               $scope.display = [];
               //loop through users information. This assumes the data we want is in response.data
 
               for (var i = 0; i < response.data.length; i++){
                 //format user's information. Assumes the id, name, email are properties. Can modify, will need proprety of message  however that users data is going to be formated. 
                 var wholeUser = "Id:" + response.data[i].id +" Name: " + response.data[i].name + " Email: " + response.data[i].email ;
-                //store user's information
+                //store user's information. This will be unnecessary as we have a database. 
                 $scope.display.push(wholeUser);
               }
               
