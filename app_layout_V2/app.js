8d0b52a524c0c3d7c68b2fbf0035765e17ae0ac5
Jazz Lyles <ripley36706@gmail.com>
2015-11-17 19:45:29 -0800

    added school templates o Please enter the commit message for your changes. Lines startin

--- app_layout_V2/app.js
+++ app_layout_V2/app.js
@@ -1,39 +1,39 @@
 // Defines module for app. If there are dependencies, insert into array
 var app = angular.module("booted", [])
+    //sets up routes
     
-    // The controller
     .controller('bootListController', function($scope, $http){
     
     //Bootcamp List below. Hardcoded Test Example included. 
 	$scope.bootcampList = [
         {
-			url: './hackreactor.html',
+			url: './schools/hackreactor.html',
 			title: 'Hack Reactor',
             image_small: './images/bootcamp_images/hackreactor_small.png',
 			image: './images/bootcamp_images/hackreactor.png',
-            bio: ''
+            bio: 'Join the programming elite and learn all the cutting edge trends'
 		},
 		{
-			url: './hackreactor.html',
+			url: './schools/hackreactor.html',
 			title: 'Telegraph Academy',
             image_small: './images/bootcamp_images/tga_small.png',
 			image: './images/bootcamp_images/tga.png',
             bio: 'An extension of the Hack Reactor program with a unique approach looking to diversify the industry and expand opportunity'
 		},
         {
-			url: './devbootcamp.html',
+			url: './schools/devbootcamp.html',
 			title: 'Dev Bootcamp',
 			image: './images/bootcamp_images/devboot_small.png',
             bio: '19-week immersive course focusing on multiple languages to give you a competitive edge on the job search'
 		},
         {
-			url: './makersquare.com',
+			url: './schools/makersquare.html',
 			title: 'Maker Square',
 			image: './images/bootcamp_images/makersquare_small.png',
             bio: 'A program designed to teach you CS fundamentals and software engineering'
 		},
         {
-			url: './hackbright.com',
+			url: './schools/hackbright.html',
 			title: 'Hackbright Academy',
 			image: './images/bootcamp_images/hackbright_small.jpg',
             bio: 'A program focused on giving women a leg up in tech'
